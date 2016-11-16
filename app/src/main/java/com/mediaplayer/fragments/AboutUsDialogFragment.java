4dae4e3e4338104749769c1f4097b359fa310675
gasahu <simplygaurav07@gmail.com>
2016-11-16 19:09:43 +0530

    Added Rate, Share and About sections

---
+++ app/src/main/java/com/mediaplayer/fragments/AboutUsDialogFragment.java
@@ -0,0 +1,19 @@
+package com.mediaplayer.fragments;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+public class AboutUsDialogFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("About");
+        builder.setMessage("Developed By:- Gaurav Sahu");
+
+        return builder.create();
+    }
+}
