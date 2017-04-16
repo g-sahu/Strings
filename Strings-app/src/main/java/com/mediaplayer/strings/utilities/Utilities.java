a777087a7ec7c3e1abaebda174adca124f6369c8
g-sahu <simplygaurav07@gmail.com>
2017-04-16 18:10:15 +0530

    Updated milliSecondsToTimer method to append 0 to minute when <10

--- Strings-app/src/main/java/com/mediaplayer/strings/utilities/Utilities.java
+++ Strings-app/src/main/java/com/mediaplayer/strings/utilities/Utilities.java
@@ -11,11 +11,11 @@ public class Utilities {
      * */
     public static String milliSecondsToTimer(long milliseconds){
         String finalTimerString = "";
-        String secondsString;
+        String minutesString, secondsString;
 
         // Convert total duration into time
-        int hours = (int)( milliseconds / (1000*60*60));
-        int minutes = (int)(milliseconds % (1000*60*60)) / (1000*60);
+        int hours = (int) (milliseconds / (1000*60*60));
+        int minutes = (int) (milliseconds % (1000*60*60)) / (1000*60);
         int seconds = (int) ((milliseconds % (1000*60*60)) % (1000*60) / 1000);
 
         // Add hours if there
@@ -23,13 +23,21 @@ public class Utilities {
             finalTimerString = hours + ":";
         }
 
+        // Prepending 0 to minutes if it is one digit
+        if(minutes < 10){
+            minutesString = "0" + minutes;
+        } else {
+            minutesString = "" + minutes;
+        }
+
         // Prepending 0 to seconds if it is one digit
         if(seconds < 10){
             secondsString = "0" + seconds;
-        }else{
-            secondsString = "" + seconds;}
+        } else {
+            secondsString = "" + seconds;
+        }
 
-        finalTimerString = finalTimerString + minutes + ":" + secondsString;
+        finalTimerString = finalTimerString + minutesString + ":" + secondsString;
 
         // return timer string
         return finalTimerString;
