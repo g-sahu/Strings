af64a7281a36b47b3fdb59ce30088983680d26e5
Gaurav Sahu <contact.gauravsahu@gmail.com>
2019-03-01 00:16:19 +0530

    Fix for restrictions on implicit broadcasts in Android O

--- Strings-app/src/main/java/com/mediaplayer/strings/services/MediaManagerService.java
+++ Strings-app/src/main/java/com/mediaplayer/strings/services/MediaManagerService.java
@@ -1,16 +1,32 @@
 package com.mediaplayer.strings.services;
 
 import android.app.IntentService;
+import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
-import com.mediaplayer.strings.utilities.MediaLibraryManager;
-import com.mediaplayer.strings.utilities.MediaPlayerConstants;
+import static android.support.v4.content.LocalBroadcastManager.*;
+import static android.support.v4.content.LocalBroadcastManager.getInstance;
+import static com.mediaplayer.strings.utilities.MediaLibraryManager.init;
+import static com.mediaplayer.strings.utilities.MediaPlayerConstants.ACTION_INIT_COMPLETE;
+import static com.mediaplayer.strings.utilities.MediaPlayerConstants.FLAG_LIBRARY_CHANGED;
 
 public class MediaManagerService extends IntentService {
+    private BroadcastReceiver broadcastReceiver;
 
     public MediaManagerService() {
         super("MediaManagerService");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        broadcastReceiver = new MediaPlayerReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_INIT_COMPLETE);
+        getInstance(this).registerReceiver(broadcastReceiver, filter);
     }
 
     @Override
@@ -18,12 +34,18 @@ public class MediaManagerService extends IntentService {
         Log.d("MediaManagerService", "Inside MediaManagerService");
 
         //Initialising/Updating Mediaplayer library
-        boolean isChanged = MediaLibraryManager.init(this);
+        boolean isChanged = init(this);
 
         //Sending broadcast indicating that MediaManagerService has finished initiliasing/updating the library
         Intent broadcastIntent = new Intent();
-        broadcastIntent.setAction(MediaPlayerConstants.ACTION_INIT_COMPLETE);
-        broadcastIntent.putExtra(MediaPlayerConstants.FLAG_LIBRARY_CHANGED, isChanged);
-        sendBroadcast(broadcastIntent);
+        broadcastIntent.setAction(ACTION_INIT_COMPLETE);
+        broadcastIntent.putExtra(FLAG_LIBRARY_CHANGED, isChanged);
+        getInstance(this).sendBroadcast(broadcastIntent);
+    }
+
+    @Override
+    public void onDestroy() {
+        getInstance(this).unregisterReceiver(broadcastReceiver);
+        super.onDestroy();
     }
 }
